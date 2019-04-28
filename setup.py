2c393d8c42d081aa668915f326ac42a8f157442b
Eduardo Dobay <edudobay@gmail.com>
2019-04-28 00:10:15 -0300

    Prepare for PyPI upload

--- setup.py
+++ setup.py
@@ -3,22 +3,38 @@ from setuptools import setup
 import simple_git_deploy
 
 
+def readfile(name):
+    with open(name) as f:
+        return f.read()
+
+README = readfile('README.md')
+
 setup(
     name='simple-git-deploy',
     version=simple_git_deploy.__version__,
-    description='Script for managing deployments from Git repositories',
+    description='A command-line utility for easy and reliable management of manual deployments from Git repositories',
+    long_description=README,
+    long_description_content_type='text/markdown',
     url='https://github.com/edudobay/simple-git-deploy',
-    license='MIT',
+    license='GPLv3',
     author='Eduardo Dobay',
     author_email='edudobay@gmail.com',
+    project_urls={
+        'Source': 'https://github.com/edudobay/simple-git-deploy',
+        'Issue Tracker': 'https://github.com/edudobay/simple-git-deploy/issues',
+    },
     classifiers=[
-        'Development Status :: 1 - Planning',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: Software Development :: Version Control :: Git',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Natural Language :: English',
         'Operating System :: OS Independent',
-        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
     ],
+    keywords='deployment automation git',
+    python_requires='>=3.5',
     install_requires=[
         'dateparser>=0.7.0',
         'inquirer',
